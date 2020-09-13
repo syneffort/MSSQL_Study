1ffdc8420c5776f879dc4c291d488b37291dd083
Pyunghwa Kim <syneffort@gmail.com>
2020-09-13 17:02:05 +0900

    Nested Loop

---
+++ 00_Sources/튜닝/7. Nested Loop Join.sql
@@ -0,0 +1,38 @@
+USE BaseballData;
+
+-- 조인의 원리
+	-- 1) Nested Loop (NL) 조인
+	-- 2) Merge (병합) 조인
+	-- 3) Hash (해시) 조인
+
+-- Merge
+SELECT *
+FROM players AS p
+	INNER JOIN salaries AS s
+	ON p.playerID = s.playerID;
+
+-- NL
+SELECT TOP 5 *
+FROM players AS p
+	INNER JOIN salaries AS s
+	ON p.playerID = s.playerID;
+
+-- Hash
+SELECT *
+FROM salaries AS s
+	INNER JOIN teams AS t
+	ON s.teamID = t.teamID;
+
+-- NL
+SELECT *
+FROM players AS p
+	INNER JOIN salaries AS s
+	ON p.playerID = s.playerID
+	OPTION(LOOP JOIN);
+
+-- 조인 순서 강제 (느려짐, salaries 테이블의 clustered index 우선하여 사용 못하게 됨)
+SELECT *
+FROM players AS p
+	INNER JOIN salaries AS s
+	ON p.playerID = s.playerID
+	OPTION(FORCE ORDER, LOOP JOIN);
---
+++ 00_Sources/튜닝/7. Nested Loop Join/TestConsole/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestConsole
+{
+    class Player
+    {
+        public int playerId;
+        //..
+    }
+
+    class Salary
+    {
+        public int PlayerId;
+        //..
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            List<Player> players = new List<Player>();
+            for (int i = 0; i < 1000; i++)
+            {
+                if (rand.Next(0, 2) == 0)
+                    continue;
+                
+                players.Add(new Player() { playerId = i });
+            }
+
+            //List<Salary> salaries = new List<Salary>();
+            Dictionary<int, Salary> salaries = new Dictionary<int, Salary>();
+            for (int i = 0; i < 1000; i++)
+            {
+                if (rand.Next(0, 2) == 0)
+                    continue;
+
+                salaries.Add(i, new Salary() { PlayerId = i });
+            }
+
+            // Q) ID가 players에도 있고 salaries에도 존재하는 정보 추출?
+
+            // Nested(내포하는)
+            List<int> result = new List<int>();
+            // result를 최대 5개만 찾는다면?
+
+
+            // O(N^2) -> O(N)
+            foreach (Player p in players)
+            {
+                /* foreach (Salary s in salaries)
+                {
+                    if (s.PlayerId == p.playerId)
+                        result.Add(p.playerId);
+
+                        break;
+                } */
+
+                Salary s = null;
+                if (salaries.TryGetValue(p.playerId, out s))
+                {
+                    result.Add(p.playerId);
+                    if (result.Count == 5)
+                        break;
+                }
+                    
+            }
+        }
+    }
+}
